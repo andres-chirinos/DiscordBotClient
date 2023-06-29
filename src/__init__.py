c351d227197c0de44d9abcd80288256c83e77b79
andres-chirinos <andreschirinos590@gmail.com>
2023-06-29 16:34:16 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,7 @@ Cache = redis.from_url(
     ),
     decode_responses=True,
 )
-guild_id = int(Cache.hget("appdata", "guild_id"))
+guild_id = os.environ.get("DISCORD_GUILD_ID")
 
 
 # Aplicacion Discord
